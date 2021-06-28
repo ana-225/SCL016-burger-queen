af8ad3287e5ccd01d39bfd0af9d3b2544733a3e5
NellyGonzalezB <nellygonzalezb.92@gmail.com>
2021-06-28 18:04:58 -0400

    prueba

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/Header'
 import Logo from './components/Logo'
 // import Main from './components/Main'
@@ -7,6 +13,11 @@ import Order from './components/Order'
 
 function App() {
   return (
+  <Router>
+    <Switch>
+      <Route path = '/login'></Route>
+      <Route path = '/lunch'></Route>
+      <Route path = '/drinks'></Route>
     <div className='background'>
       <Header/>
       <Lunch/>
@@ -15,6 +26,8 @@ function App() {
       <Lunch/>
       <Order/>
     </div>
+    </Switch>
+    </Router>
   );
 }
 

dd72119fee45cc9569693d8335f8c235d0198f3e
ana-225 <ana-225@gmail.com>
2021-06-27 21:25:55 -0400

    creacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Logo from './components/Logo'
+// import Main from './components/Main'
+import Lunch from './components/Lunch'
+import Order from './components/Order'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <Header/>
+      <Lunch/>
+      <Lunch/>
+      <Lunch/>
+      <Lunch/>
+      <Order/>
     </div>
   );
 }

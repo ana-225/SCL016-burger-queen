ae488caed5be86b7702c6b2f87a7e00bad7dde09
ana-225 <ana-225@gmail.com>
2021-06-28 17:47:13 -0400

    Creacion de rutas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/Header'
-import Logo from './components/Logo'
+//import Logo from './components/Logo'
 // import Main from './components/Main'
+
 import Lunch from './components/Lunch'
 import Order from './components/Order'
 
 function App() {
   return (
-    <div className='background'>
-      <Header/>
-      <Lunch/>
-      <Lunch/>
-      <Lunch/>
-      <Lunch/>
-      <Order/>
+    <Router>
+     <div className='background'>
+       <Switch>
+        <Route path ='/' exact>
+        </Route>
+        <Route path = '/lunch'>
+          <Header/>
+          <Lunch/>
+          <Lunch/>
+          <Lunch/>
+          <Lunch/>
+          <Order/>
+        </Route>
+        <Route path = '/drinks'>
+        <Lunch/>
+        </Route>
+    
+    
+    </Switch>
     </div>
+    </Router>
   );
 }
 

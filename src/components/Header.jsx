0ee6d43e805651270b63a0003d1b6a8b1618dcb2
ana-225 <ana-225@gmail.com>
2021-07-07 12:09:53 -0400

    Se agrega boton para ingresar datos a firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ const Header = () => {
   return ( 
   <div className = 'header-container'>
       <div className = 'client-name'>
-        <input type = 'form' placeholder = 'NOMBRE CLIENTE' className = 'clientInfo'></input> <button className = 'add-btn'>AGREGAR</button>
+        <input type = 'form' placeholder = 'NOMBRE CLIENTE' className = 'clientInfo'></input> 
       </div>
       <div className = 'table-number'>
-        <input type = 'form' placeholder = 'N° MESA' className = 'tableInfo'></input> <button className = 'add-btn'>AGREGAR</button>
+        <input type = 'form' placeholder = 'N° MESA' className = 'tableInfo'></input>
       </div>
     <div>
       <button className = 'return-btn' onClick={() => window.location.href = "http://localhost:3000/"}>
-        <img src='https://i.ibb.co/ZdR2ZG0/back.png' alt= ''/>
+        <img src='https://firebasestorage.googleapis.com/v0/b/burger-queen-fee8c.appspot.com/o/Im%C3%A1genes%20Men%C3%BA%2Farrow-right.png?alt=media&token=5e79bd8a-3d25-4197-a66c-8b24a16f6490' alt= ''/>
       </button>
     </div>
   </div>
